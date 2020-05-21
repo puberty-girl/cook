65217c2710a883950da58a26603d10e8740dbff6
criterionsz <ed371612>
2020-05-21 09:48:39 +0300

    изменения в классе recipes

--- app/src/main/java/com/example/cook2/RecipesActivity.java
+++ app/src/main/java/com/example/cook2/RecipesActivity.java
@@ -36,42 +36,8 @@ public class RecipesActivity extends AppCompatActivity {
             imageString = extras.getString("image");
             description = extras.getString("description");
         }
-
         myText.setText(description);
         Glide.with(getApplicationContext()).asBitmap().load(imageString)//откуда идет
                 .into(image);
-
-       // Recipes myData = (Recipes) extras.getSerializable("KEY");
-
-        //downloadData(SingleRetrofit.getInstance().createApiSample(), id);
-    }
-
-
-    public void downloadData (RecipesApi recipesApi, int id){
-
-        Call<Recipes> recipes = recipesApi.recipe(id);
-
-        recipes.enqueue(new Callback<Recipes>() {
-            @Override
-            public void onResponse(Call<Recipes> call, Response<Recipes> response) {
-                if (response.isSuccessful()) {
-                    recipe = response.body();
-                    myText.setText(recipe.getName());
-                    Glide.with(getApplicationContext()).asBitmap().load(recipe.getImage())//откуда идет
-                            .into(image);
-                    //Toast.makeText(getApplicationContext(), recipe.getName(), Toast.LENGTH_LONG);
-                    //myText.setText(recipe.getName());
-
-                    Log.d(TAG, "response Id:" + recipe.getId() + " Name:" + recipe.getName() + " Id_type:" + recipe.getId_type());
-                } else {
-                    Log.d(TAG, "response code " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Recipes> call, Throwable t) {
-                Log.d(TAG, "failure " + t);
-            }
-        });
     }
 }
